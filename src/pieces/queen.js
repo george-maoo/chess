8ec40d38f66f19d91a9f8bba4d0e689c0d0b54e5
george <georgemao72@gmail.com>
2023-11-03 17:49:29 -0700

    Put multiple move generator in separate file

--- src/pieces/queen.js
+++ src/pieces/queen.js
@@ -1,4 +1,5 @@
 const Piece = require("./piece");
+const moveType = require("./utils/movetype");
 
 class Queen extends Piece {
   string_rep() {
@@ -18,42 +19,8 @@ class Queen extends Piece {
     ];
   }
 
-  // for all pieces that can move multiple tiles:
-  /*
-  for every move in validMoves, check every possible move in that direction
-  until you hit:
-    - another piece
-      - if piece is same color:
-        - dont add the position as a valid move
-      - if piece is a enemy:
-        - add the enemy piece's location as a valid move
-    - move goes out of bounds
-  */
   validMoves() {
-    const moves = [];
-
-    this.moveDirections().forEach(([rowDir, colDir]) => {
-      let [rowToCheck, colToCheck] = this.location;
-
-      while (true) {
-        rowToCheck += rowDir;
-        colToCheck += colDir;
-        const posToCheck = [rowToCheck, colToCheck];
-
-        if (!this.board.isInBounds(posToCheck)) break;
-
-        if (this.board.board[rowToCheck][colToCheck] === null) {
-          moves.push(posToCheck);
-        } else {
-          if (this.board.board[rowToCheck][colToCheck].color !== this.color) {
-            moves.push(posToCheck);
-          }
-          break;
-        }
-      }
-    });
-
-    return moves;
+    return moveType.multiMove(this);
   }
 }
 
