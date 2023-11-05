5d799a631baab46e83f6180cd6f5037af98d1640
george <georgemao72@gmail.com>
2023-11-05 00:38:09 -0700

    Refactor/add tests for Game class

--- src/game.js
+++ src/game.js
@@ -1,58 +1,28 @@
-const readline = require("node:readline/promises");
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
 class Game {
   constructor(board) {
     this.board = board;
     this.currentPlayer = "white";
   }
 
-  async play() {
-    const { board } = this;
-
-    while (!this.gameOver()) {
-      await this.playTurn();
-      this.switchTurns();
+  attemptMove({ startPos, endPos }) {
+    if (this.board.atLocation(startPos).color !== this.currentPlayer) {
+      return;
+    }
+    if (!this.board.movePiece(startPos, endPos)) {
+      return;
     }
 
     this.switchTurns();
-    board.printBoard();
-    console.log(`Game over, ${this.currentPlayer} wins!`);
-    rl.close();
+
+    if (this.gameOver()) {
+      this.switchTurns();
+      return this.currentPlayer;
+    }
   }
 
   switchTurns() {
     const playerToSwitchTo = this.currentPlayer === "white" ? "black" : "white";
     this.currentPlayer = playerToSwitchTo;
-  }
-
-  parseUserInput(userInput) {
-    return userInput.split(" ").map((x) => parseInt(x));
-  }
-
-  // youre joking i need async programming for user input??????
-  async playTurn() {
-    const { board, parseUserInput, currentPlayer } = this;
-
-    while (true) {
-      board.printBoard();
-      console.log(`It is now ${currentPlayer}'s turn`);
-      if (board.isInCheck()) console.log("You are in check");
-
-      const startPos = await rl.question("Enter a starting position: \n");
-      const endPos = await rl.question("Enter a ending position: \n");
-
-      if (board.movePiece(parseUserInput(startPos), parseUserInput(endPos))) {
-        console.log(`${currentPlayer} moved from ${startPos} to ${endPos}`);
-        break;
-      }
-
-      console.log("Invalid move");
-    }
   }
 
   gameOver() {
