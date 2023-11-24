5ce1111a124281ec97d3ebc1634d21553d4a4bfe
dotaplayer1 <georgemao72@gmail.com>
2023-11-23 18:38:52 -0800

    Fix castling

--- src/pieces/movetypes/castleMove.js
+++ src/pieces/movetypes/castleMove.js
@@ -38,13 +38,12 @@ const castleMove = (king) => {
   const rightRook = board.atLocation([kingRow, kingCol + 3]);
 
   // pair of rook and which direction it is from the king
-  const rooks = [leftRook, rightRook];
-  const offsets = [-1, 1];
+  const pairs = [
+    [leftRook, -1],
+    [rightRook, 1],
+  ];
 
-  for (let i = 0; i < 2; i++) {
-    const rook = rooks[i];
-    const offset = offsets[i];
-
+  for (const [rook, offset] of pairs) {
     // check if rook moved and rook can move to spot right beside king
     if (
       rook &&
